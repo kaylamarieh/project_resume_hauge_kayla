b25c5ae509f35cf26eee72f1ee0a4e14638aa935
Kayla Hauge <kmhauge@ufl.edu>
2018-04-15 18:54:39 -0400

    add jquery to fade in section text

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
 
+    var windowSize = $(window).height();
+
+    //scrollify
     $.scrollify({
         section: '.full-height',
     });
 
-    $('#text').ShuffleText([
+    //shuffle text
+    $('#text').shuffleText([
         'Web Developer',
         'Politics Obsessor',
         'Digital Designer',
@@ -13,5 +17,21 @@ $(document).ready(function () {
         'Unicorn Lover',
         'News Consumer'
     ], {loop: true});
+
+    //section text fade in
+    $(window).scroll(function () {
+        var y = window.scrollY;
+        if (y > 0 && y <= windowSize) {
+            $('#edu article').css({'transition': 'opacity 0.8s ease-in', 'opacity': '1'});
+        } else if (y > windowSize && y <= windowSize * 2) {
+            $('#work article').css({'transition': 'opacity 0.8s ease-in', 'opacity': '1'});
+        } else if (y > windowSize * 2 && y <= windowSize * 3) {
+            $('#skills article').css({'transition': 'opacity 0.8s ease-in', 'opacity': '1'});
+        } else if (y > windowSize * 3 && y <= windowSize * 4) {
+            $('#interests article').css({'transition': 'opacity 0.8s ease-in', 'opacity': '1'});
+        } else if (y > windowSize * 4 && y <= windowSize * 5) {
+            $('footer p, footer ul').css({'transition': 'opacity 0.8s ease-in', 'opacity': '1'});
+        }
+    });
 
 });
