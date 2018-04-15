f839a1a817ae11887969726096c286b106766867
Kayla Hauge <kmhauge@ufl.edu>
2018-04-15 12:48:39 -0400

    add shuffle text jquery plugin

--- js/main.js
+++ js/main.js
@@ -4,4 +4,14 @@ $(document).ready(function () {
         section: '.full-height',
     });
 
+    $('#text').ShuffleText([
+        'Web Developer',
+        'Politics Obsessor',
+        'Digital Designer',
+        'Podcast Listener',
+        'Photographer',
+        'Unicorn Lover',
+        'News Consumer'
+    ], {loop: true});
+
 });
